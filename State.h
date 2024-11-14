aa9cadfeadb19467bbab81a4e05bfede4cd6dbc9
JingmingLiu <jingmingliu01@outlook.com>
2024-11-14 16:19:38 -0500

    P2: delete some redundant code.

--- State.h
+++ State.h
@@ -1,6 +1,3 @@
-//
-// Created by Jimmy on 2024-11-11.
-//
 
 #ifndef STATE_H
 #define STATE_H
@@ -53,23 +50,3 @@ public:
 inline ostream& operator<<(ostream& os, const Square& square) { return square.print(os); }
 
 #endif //STATE_H
-
-class Square {
-private:
-    State state;
-    short row;
-    short col;
-    vector<Square> neighbors;
-
-public:
-    Square() = default;
-
-    Square(char c, short row, short col);;
-    ~Square() { cerr << "Deleting Square" << '[' << row << ',' << col << ']' << endl; };
-
-    void mark(char c);
-
-    ostream& print(ostream& os) const;;
-};
-
-inline ostream& operator<<(ostream& os, const Square& square) { return square.print(os); }
